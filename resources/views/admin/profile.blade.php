eeef157c301c43e9f344ccdcc8a183abbd0ff092
PNVD203\Administrator <trancongdung12@gmail.com>
2021-03-10 17:58:51 +0700

    update test

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -13,9 +13,9 @@
   <table class="table table-hover">
   <thead>
   </thead>
-  <th>Full name D</th>
+  <th>Full name </th>
   <th>Email</th>
-  <th></th>
+  <th>ID</th>
   <th></th>
   <th></th>
   <th></th>
