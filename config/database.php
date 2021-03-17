1318e149c7762e7f3523663615867d5f9582627e
hieppn <hiep.bui21@student.passerellesnumeriques.org>
2021-03-17 15:32:57 +0700

    fix time bug

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'timezone'  => '+00:00',
+            'timezone'  => 'Asia/Ho_Chi_Minh',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
