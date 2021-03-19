7688d24c945c168c540ab37824be0b6fd6c52b39
hieppn <hiep.bui21@student.passerellesnumeriques.org>
2021-03-19 09:40:24 +0700

    order mir

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -20,9 +20,9 @@ class OrderSeeder extends Seeder
             'send_from'=>'Quang Nam',
             'send_to'=>'Da Nang',
             'time_send'=>'2020-07-06 18:20:40',
-            'name'=> " Chuyen nha tro bao gom cac do dung",
+            'name'=> "Chuyển trọ",
             'mass'=>100,
-            'car_type'=>'xe tair 4 banh',
+            'car_type'=>'Xe 3 bánh',
             'export_data'=>true,
             'image'=>'ord1.png',
             'type'=>'new',
@@ -31,87 +31,6 @@ class OrderSeeder extends Seeder
             'receiver_info'=>'Huong 102',
             'id_user'=>1,
             
-        ]);
-
-        DB::table('orders')->insert([
-            
-            'send_from'=>'Quang Nam',
-            'send_to'=>'Da Nang',
-            'time_send'=>'2020-07-06 18:20:40',
-            'name'=> " Chuyen nha tro bao gom cac do dung",
-            'mass'=>100,
-            'car_type'=>'xe tair 4 banh',
-            'export_data'=>true,
-            'image'=>'ord1.png',
-            'type'=>'new',
-            'price'=>'12000000',
-            'sender_info'=>'Huong 101',
-            'receiver_info'=>'Huong 102',
-            'id_user'=>2,
-        ]);
-        DB::table('orders')->insert([
-            
-            'send_from'=>'Quang Nam',
-            'send_to'=>'Da Nang',
-            'time_send'=>'2020-07-06 18:20:40',
-            'name'=> " Chuyen nha tro bao gom cac do dung",
-            'mass'=>100,
-            'car_type'=>'xe tair 4 banh',
-            'export_data'=>true,
-            'image'=>'ord1.png',
-            'type'=>'new',
-            'price'=>'12000000',
-            'sender_info'=>'Huong 101',
-            'receiver_info'=>'Huong 102',
-            'id_user'=>2,
-        ]);
-        DB::table('orders')->insert([
-            
-            'send_from'=>'Quang Nam',
-            'send_to'=>'Da Nang',
-            'time_send'=>'2020-07-06 18:20:40',
-            'name'=> " Chuyen nha tro bao gom cac do dung",
-            'mass'=>100,
-            'car_type'=>'xe tair 4 banh',
-            'export_data'=>true,
-            'image'=>'ord1.png',
-            'type'=>'new',
-            'price'=>'12000000',
-            'sender_info'=>'Huong 101',
-            'receiver_info'=>'Huong 102',
-            'id_user'=>2,
-        ]);
-        DB::table('orders')->insert([
-            
-            'send_from'=>'Quang Nam',
-            'send_to'=>'Da Nang',
-            'time_send'=>'2020-07-06 18:20:40',
-            'name'=> " Chuyen nha tro bao gom cac do dung",
-            'mass'=>100,
-            'car_type'=>'xe tair 4 banh',
-            'export_data'=>true,
-            'image'=>'ord1.png',
-            'type'=>'new',
-            'price'=>'12000000',
-            'sender_info'=>'Huong 101',
-            'receiver_info'=>'Huong 102',
-            'id_user'=>2,
-        ]);
-        DB::table('orders')->insert([
-            
-            'send_from'=>'Quang Nam',
-            'send_to'=>'Da Nang',
-            'time_send'=>'2020-07-06 18:20:40',
-            'name'=> " Chuyen nha tro bao gom cac do dung",
-            'mass'=>100,
-            'car_type'=>'xe tair 4 banh',
-            'export_data'=>true,
-            'image'=>'ord1.png',
-            'type'=>'new',
-            'price'=>'12000000',
-            'sender_info'=>'Huong 101',
-            'receiver_info'=>'Huong 102',
-            'id_user'=>2,
         ]);
     }
 }
