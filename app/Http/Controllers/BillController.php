591433b54b912877affd9e9a25d83b61f3bdb6b3
unknown <Administrator@PNVD190.pn.com.vn>
2021-04-09 17:05:04 +0700

    add orderby

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -18,7 +18,8 @@ class BillController extends Controller
         (SELECT t.name FROM trucks as t WHERE t.id = b.id_truck ) AS truck_name ,
         (SELECT t.license_plate FROM trucker_information AS t WHERE t.id_trucker = b.id_trucker ) AS plate 
         FROM bills AS b, orders AS o, users AS u 
-        WHERE b.id_order = o.id AND b.id_trucker = u.id;'),200);
+        WHERE b.id_order = o.id AND b.id_trucker = u.id
+        ORDER BY b.id DESC'),200);
     }
     public function getBillByIdTruck(Request $request,$id){
         return response()->json(DB::select('select b.id_trucker, o.* from bills as b, orders as o where  o.id = b.id_order  and o.type = 2 and b.id_trucker = '.$id),200);
--- app/Http/Controllers/SearchHistoryController.php
+++ app/Http/Controllers/SearchHistoryController.php
@@ -14,7 +14,7 @@ class SearchHistoryController extends Controller
         return response()->json('Success', 200);    
     }
     public function getSearchByIdUser(Request $request, $id){
-        $searchHistory = SearchHistory::where('id_user', $id)->get();
+        $searchHistory = SearchHistory::where('id_user', $id)->orderBy('id', 'DESC')->get();
         return response()->json($searchHistory,200);
     }
     public function delete(Request $request, $id){
@@ -22,7 +22,7 @@ class SearchHistoryController extends Controller
         $search = SearchHistory::find($id);
         $id_user = $search->id_user;
         $search->delete();
-        $result = SearchHistory::where('id_user',$id_user)->get();
+        $result = SearchHistory::where('id_user',$id_user)->orderBy('id', 'DESC')->get();
         return response()->json($result,200);
     }
 }
