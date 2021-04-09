11bb9a63fdcb76b56bf52b705a6004aa7d4918aa
unknown <Administrator@PNVD190.pn.com.vn>
2021-04-09 17:06:31 +0700

    add orderby

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -15,7 +15,7 @@ class BillController extends Controller
     }
     public function getBillById(Request $request,$id){
         return response()->json(DB::select('SELECT o.*,u.full_name AS trucker_name,u.phone, 
-        (SELECT t.name FROM trucks as t WHERE t.id = b.id_truck ) AS truck_name ,
+        (SELECT t.name FROM trucks as t WHERE t.id = b.id_truck ) AS truck ,
         (SELECT t.license_plate FROM trucker_information AS t WHERE t.id_trucker = b.id_trucker ) AS plate 
         FROM bills AS b, orders AS o, users AS u 
         WHERE b.id_order = o.id AND b.id_trucker = u.id
