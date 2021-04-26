c52faeb03989332c08e826090ffece45312cb81c
hieppn <hiep.bui21@student.passerellesnumeriques.org>
2021-04-26 20:10:14 +0700

    chart

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -17,7 +17,8 @@ class ChartController extends Controller
      */
     public function index()
     {
-            $post=Order::select(Order::raw('extract(month from "created_at") as month'),Order::raw('COUNT(id) as sum'))
+            $post=Order::select(Order::raw('MONTH(created_at) as month'),Order::raw('COUNT(id) as sum'))
+            ->whereYear('created_at', now()->year)
             ->groupBy('month')->get(); 
             $postmonth=[0,0,0,0,0,0,0,0,0,0,0,0];
             foreach($post as $post){
@@ -32,11 +33,12 @@ class ChartController extends Controller
 
     public function getLineUser(){
       
-            $user=User::select(User::raw('extract(month from "created_at") as month'),User::raw('COUNT(id) as sum'))
+            $user=User::select(User::raw('MONTH(created_at) as month'),User::raw('COUNT(id) as sum'))
+            ->whereYear('created_at', now()->year)
             ->where('id_role', 1)
             ->orWhere('id_role', 2)
             ->groupBy('month')->get();   
-            $user_month=[0,0,0,0,0,0,0,0,0,0,0,0];
+            $user_month = array(0,0,0,0,0,0,0,0,0,0,0,0);
             foreach($user as $user){
             for($i=1;$i<=12;$i++){
               if($i==$user["month"]){
