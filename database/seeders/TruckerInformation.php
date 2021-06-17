88c028844504c6d677e6539a6cbc458f95ae787c
hieppn <hiep.bui21@student.passerellesnumeriques.org>
2021-06-17 11:00:13 +0700

    game over

--- database/seeders/TruckerInformation.php
+++ database/seeders/TruckerInformation.php
@@ -15,34 +15,34 @@ class TruckerInformation extends Seeder
     {
         DB::table('trucker_information')->insert([
         'id_trucker' =>4 ,
-        'id_card_front' => 'https://dev-dtravel-data.s3.ap-northeast-1.amazonaws.com/images/1618993610-Ya6QMTH4UL607fe1ca59fd8',
-        'id_card_back' => 'https://dev-dtravel-data.s3.ap-northeast-1.amazonaws.com/images/1618994660-FtNzAahtNU607fe5e429659',
-        'license_front' => 'https://dev-dtravel-data.s3.ap-northeast-1.amazonaws.com/images/1618994744-Fl8YT1cAhy607fe6381dffc',
-        'license_back' => 'https://dev-dtravel-data.s3.ap-northeast-1.amazonaws.com/images/1618994835-AkQsbbgiZv607fe6934aefe',
+        'id_card_front' => 'https://d3i71xaburhd42.cloudfront.net/ecfbcceb44a156ac51aaf452b90c1155f4da2f44/2-Figure2-1.png',
+        'id_card_back' => 'https://d3i71xaburhd42.cloudfront.net/ecfbcceb44a156ac51aaf452b90c1155f4da2f44/2-Figure2-1.png',
+        'license_front' => 'https://d3i71xaburhd42.cloudfront.net/ecfbcceb44a156ac51aaf452b90c1155f4da2f44/2-Figure2-1.png',
+        'license_back' => 'https://d3i71xaburhd42.cloudfront.net/ecfbcceb44a156ac51aaf452b90c1155f4da2f44/2-Figure2-1.png',
         'license_plate' => '77 H1 5678',
-        'registration_paper'=>'https://dev-dtravel-data.s3.ap-northeast-1.amazonaws.com/images/1618994877-6nRQeM1cen607fe6bd6a5f5',
+        'registration_paper'=>'https://d3i71xaburhd42.cloudfront.net/ecfbcceb44a156ac51aaf452b90c1155f4da2f44/2-Figure2-1.png',
         'car_type' =>'Xe 3 bánh',
         'payload' => 3.6
         ]);
         DB::table('trucker_information')->insert([
             'id_trucker' =>5 ,
-            'id_card_front' => 'https://dev-dtravel-data.s3.ap-northeast-1.amazonaws.com/images/1618993610-Ya6QMTH4UL607fe1ca59fd8',
-            'id_card_back' => 'https://dev-dtravel-data.s3.ap-northeast-1.amazonaws.com/images/1618994660-FtNzAahtNU607fe5e429659',
-            'license_front' => 'https://dev-dtravel-data.s3.ap-northeast-1.amazonaws.com/images/1618994744-Fl8YT1cAhy607fe6381dffc',
-            'license_back' => 'https://dev-dtravel-data.s3.ap-northeast-1.amazonaws.com/images/1618994835-AkQsbbgiZv607fe6934aefe',
+            'id_card_front' => 'https://d3i71xaburhd42.cloudfront.net/ecfbcceb44a156ac51aaf452b90c1155f4da2f44/2-Figure2-1.png',
+            'id_card_back' => 'https://d3i71xaburhd42.cloudfront.net/ecfbcceb44a156ac51aaf452b90c1155f4da2f44/2-Figure2-1.png',
+            'license_front' => 'https://d3i71xaburhd42.cloudfront.net/ecfbcceb44a156ac51aaf452b90c1155f4da2f44/2-Figure2-1.png',
+            'license_back' => 'https://d3i71xaburhd42.cloudfront.net/ecfbcceb44a156ac51aaf452b90c1155f4da2f44/2-Figure2-1.png',
             'license_plate' => '77 H1 5678',
-            'registration_paper'=>'https://dev-dtravel-data.s3.ap-northeast-1.amazonaws.com/images/1618994877-6nRQeM1cen607fe6bd6a5f5',
+            'registration_paper'=>'https://d3i71xaburhd42.cloudfront.net/ecfbcceb44a156ac51aaf452b90c1155f4da2f44/2-Figure2-1.png',
             'car_type' =>'Xe 3 bánh',
             'payload' => 3.6
             ]);
         DB::table('trucker_information')->insert([
             'id_trucker' =>8 ,
-            'id_card_front' => 'https://dev-dtravel-data.s3.ap-northeast-1.amazonaws.com/images/1618993610-Ya6QMTH4UL607fe1ca59fd8',
-            'id_card_back' => 'https://dev-dtravel-data.s3.ap-northeast-1.amazonaws.com/images/1618994660-FtNzAahtNU607fe5e429659',
-            'license_front' => 'https://dev-dtravel-data.s3.ap-northeast-1.amazonaws.com/images/1618994744-Fl8YT1cAhy607fe6381dffc',
-            'license_back' => 'https://dev-dtravel-data.s3.ap-northeast-1.amazonaws.com/images/1618994835-AkQsbbgiZv607fe6934aefe',
+            'id_card_front' => 'https://d3i71xaburhd42.cloudfront.net/ecfbcceb44a156ac51aaf452b90c1155f4da2f44/2-Figure2-1.png',
+            'id_card_back' => 'https://d3i71xaburhd42.cloudfront.net/ecfbcceb44a156ac51aaf452b90c1155f4da2f44/2-Figure2-1.png',
+            'license_front' => 'https://d3i71xaburhd42.cloudfront.net/ecfbcceb44a156ac51aaf452b90c1155f4da2f44/2-Figure2-1.png',
+            'license_back' => 'https://d3i71xaburhd42.cloudfront.net/ecfbcceb44a156ac51aaf452b90c1155f4da2f44/2-Figure2-1.png',
             'license_plate' => '77 H2 5678',
-            'registration_paper'=>'https://dev-dtravel-data.s3.ap-northeast-1.amazonaws.com/images/1618994877-6nRQeM1cen607fe6bd6a5f5',
+            'registration_paper'=>'https://d3i71xaburhd42.cloudfront.net/ecfbcceb44a156ac51aaf452b90c1155f4da2f44/2-Figure2-1.png',
             'car_type' =>'Xe 3 bánh',
             'payload' => 3.6
             ]);
